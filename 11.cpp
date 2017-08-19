e23f1c7c95d46eb19cbde4e44169deaf9c71237c
GOWNTHAR4040 <30342844+GOWNTHAR4040@users.noreply.github.com>
2017-08-19 16:53:40 +0530

    Create 11.cpp

---
+++ 11.cpp
@@ -0,0 +1,10 @@
+#include<iostream.h>
+using namespace std;
+int main ()
+{ 
+   int a;
+   count<<"enter any non zero number : "
+   cin>>a;
+   (a>0)?cout<<"Number is positive":cout<<"Number is negative";
+	 return 0;
+}
